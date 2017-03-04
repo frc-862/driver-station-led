5eaafe236064e3d488ecda96798504087b219431
Marcus Stojcevich <mstojcevich@gmail.com>
2017-03-04 14:32:02 -0500

    Add actual commands

--- station.py
+++ station.py
@@ -6,6 +6,17 @@ import sys
 import logging
 from time import sleep
 
+OPERATION_SWITCH_MODE   = 0
+OPERATION_SET_COLOR     = 1
+
+MODE_SOLID      = 0
+MODE_RAINBOW    = 1
+
+COLOR_ORANGE    = 0
+COLOR_BLUE      = 1
+COLOR_GREEN     = 2
+COLOR_RED       = 3
+
 def setup_serial():
     try:
         portList = [port[0] for port in serial.tools.list_ports.comports() if port[2] != 'n/a']
@@ -27,11 +38,15 @@ def setup_network_tables():
 
     NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
 
-    sd = NetworkTables.getTable("SmartDashboard")
+    sd = NetworkTables.getTable("DashControl")
     sd.addTableListener(valueChanged)
 
 def valueChanged(table, key, value, isNew):
-    arduino.write("valueChanged: key: '%s'; value: %s; isNew: %s\n" % (key, value, isNew))
+    if key == 'station_led_color':
+        arduino.write(bytearray([OPERATION_SET_COLOR, value]))
+    if key == 'station_led_mode':
+        arduino.write(bytearray([OPERATION_SWITCH_MODE, value]))
+
     print("valueChanged: key: '%s'; value: %s; isNew: %s" % (key, value, isNew))
 
 def connectionListener(connected, info):
@@ -46,6 +61,13 @@ setup_network_tables()
 if arduino is None:
     print("Error connecting to serial port")
     sys.exit(1)  # Exit with error
+
+# Sleep to give the arduino some time
+sleep(2)
+
+# Write initial mode and color
+arduino.write(bytearray([OPERATION_SWITCH_MODE, MODE_SOLID]))
+arduino.write(bytearray([OPERATION_SET_COLOR, COLOR_BLUE]))
 
 while True:  # Keep running so NetworkTables' event loop can run
     sleep(1)
